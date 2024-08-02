b1027284704ea1a641b143b077e9bf486c533486
KINDAAbdoulLatif <kinda.abdoul.latif.14@gmail.com>
2024-08-02 16:02:03 +0200

    skill

--- app/_components/Skills.tsx
+++ app/_components/Skills.tsx
@@ -33,7 +33,7 @@ export const  Skills = ()=> {
                 <div className="flex flex-col gap-4 ">
                     <IAIcon size={32} className="animate-spin" style={{animationDuration: "10s"}}/>
                 </div>
-                <h3 className="mb-2 text-xl font-bold  tracking-tight">IA</h3>
+                <h3 className="mb-2 text-xl font-bold  tracking-tight">Big Data/AI</h3>
                 <p className="text-sm text-muted-foreground">
                     I{`'`}m an AI Engineer </p>
             </div>
